c47c305c78a002249ec8fa62c78b82388ed45d31
Viktorre <reif.viktor@gmail.com>
2022-07-10 18:13:58 +0200

    implmented test_format_movie_data()

--- tests/test_format_movie_data.py
+++ tests/test_format_movie_data.py
@@ -2,10 +2,14 @@ import os
 import pandas as pd
 from main import format_movie_id_col_and_update_dtypes
 
-def test_format_movie_data() ->None:
-    """...
-    """    
-    data = pd.read_csv("tests/data/data_movie_id_format.txt",
+
+def test_format_movie_data() -> None:
+    """Uses short sample file to check the following points in format_movie_id_col_and_update_dtypes():
+        1. Does the column "movie_id" exist?
+        2. Is the "movie_id" correctly mapped to the "user_id"?
+        3. Are the redundant rows containing the now mapped movie ids deleted?
+    """
+    data = pd.read_csv('tests/data/data_movie_id_format.txt',
                        sep=',',
                        na_values=[''],
                        names=['user_id', 'rating', 'date'],
@@ -15,5 +19,6 @@ def test_format_movie_data() ->None:
                            'date': 'string'
                        })
     formatted_data = format_movie_id_col_and_update_dtypes(data)
-    assert len(data)>0
-    
+    assert formatted_data.shape[1] == 4
+    assert formatted_data.shape[0] == 12
+    assert len(formatted_data["movie_id"].value_counts().unique()) == 1
