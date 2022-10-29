1f598ead3bf8e2771fc49cd9eea2106a35b07f96
Viktorre <reif.viktor@gmail.com>
2022-10-29 15:16:44 +0200

    typo fix in docstring

--- tests/test_load_dotenv.py
+++ tests/test_load_dotenv.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 def test_load_dotenv() ->None:
-    """Asserts that after loading env variables of this project the total number of env variables available via os is increased. This proxies that the .env import was succesfull.
+    """Asserts that after loading env variables of this project the total number of env variables available via os is increased. This proxies that the .env import was succesful.
     """    
     env_vars_before = len(os.environ)
     load_dotenv('.env.md')
