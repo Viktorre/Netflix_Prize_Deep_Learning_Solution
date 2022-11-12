08f7a2ed01a216e08a16ea51ef323eb632379b60
Viktorre <reif.viktor@gmail.com>
2022-11-12 11:41:00 +0100

    implemnted dtype asserts via pandas ptypes. refactor to do

--- tests/test_format_movie_id_col_and_update_dtypes.py
+++ tests/test_format_movie_id_col_and_update_dtypes.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pandas.api.types as ptypes
 from unittest import TestCase
 from main import format_movie_id_col_and_update_dtypes
 
@@ -6,8 +7,6 @@ from main import format_movie_id_col_and_update_dtypes
 class TestFormatMainData(TestCase):
 
     expected_column_names = ['user_id', 'rating', 'date', 'movie_id']
-
-    # expected_dtypes = [dtype('int64'), dtype('int64'), dtype('<M8[ns]'), dtype('int64')]
 
     def test_dtypes_conversion_and_column_names(self):
         df = pd.DataFrame({
@@ -23,4 +22,8 @@ class TestFormatMainData(TestCase):
         })
         df = format_movie_id_col_and_update_dtypes(df)
 
+        self.assertTrue(ptypes.is_numeric_dtype(df['user_id']))
+        self.assertTrue(ptypes.is_numeric_dtype(df['rating']))
+        self.assertTrue(ptypes.is_datetime64_any_dtype(df['date']))
+        self.assertTrue(ptypes.is_numeric_dtype(df['movie_id']))
         self.assertListEqual(df.columns.tolist(), self.expected_column_names)
